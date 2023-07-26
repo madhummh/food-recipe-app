fe48d93e8475598dd521a9c5c27432f657c0d554
madhummh <madhuhills95@gmail.com>
2023-07-26 17:50:17 +0530

    Cleaned up controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -152,7 +152,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
+  // newFeature();
 
   // controlServings();
 }
